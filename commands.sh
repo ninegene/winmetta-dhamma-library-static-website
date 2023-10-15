032c8a5e10eaccee998a51d786ce6072e78e4e40
Ubuntu <bitnami@winmetta.org>
2023-10-15 04:38:28 +0000

    chore: initial add index.html files

---
+++ commands.sh
@@ -0,0 +1,14 @@
+
+f_list=$(find . -type f -name 'index.html' -exec echo {} \;)
+IFS=$'\n'
+for f in $f_list; do
+    d=$(dirname "$f")
+    path=${d:2}
+    # https://stackoverflow.com/questions/296536/how-to-urlencode-data-for-curl-command
+    path=$(printf "$path" | jq -sRr '@uri')
+    echo "--- d: $d     f: $f    path: $path"
+    sed -re "s|<a href=\"(.+)\.pdf\"|<a href=\"https://dhamma-library.cdn.winmetta.org/$path/\1.pdf\"|g" "$f" > "$d/index.htm"
+    #[[ -f "$d/index.html.bak" ]] || mv -iv "$d/index.html" "$d/index.html.bak"
+    #mv -iv "$d/index.html.bak" "$d/index.html"
+    mv -v "$d/index.htm" "$d/index.html"
+done
